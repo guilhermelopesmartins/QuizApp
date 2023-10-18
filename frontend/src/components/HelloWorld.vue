3a339192b54e70681d746f1adb0b9279e0bc73cb
Tanner Schmoekel <tannerschmoekel@Tanners-Laptop.local>
2023-10-17 20:56:02 -0500

    first commit

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Hello World</h1>
+</template>
+
+<script setup>
+    import { ref } from 'vue'
+    const msg = ref('Hello World')
+    // calling api to manage quiz
+
+</script>
