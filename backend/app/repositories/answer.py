12b5459aa9340ee6a0830c2c9506d95889d1d34e
Tanner Schmoekel <tannerschmoekel@Tanners-Laptop.local>
2023-11-06 22:05:02 -0600

    fixes

--- backend/app/repositories/answer.py
+++ backend/app/repositories/answer.py
@@ -4,5 +4,4 @@ from core.database import models
 
 class AnswerRepository(BaseRepository[Answer]):
     def __init__(self):
-        # super().__init__(schema=QuestionOut, create_schema=QuestionIn, db_model=models.Question)
         super().__init__(schema=AnswerOut, create_schema=AnswerIn, db_model=models.Answer)
